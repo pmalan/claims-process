a5e5abce8ced920eae476d6e2d98ffdc7f611302
pmalan-rh <pmalan@redhat.com>
2021-10-26 07:47:44 -0700

    Initial

---
+++ src/main/java/com/redhat/idaas/fhir/FHIRResource.java
@@ -0,0 +1,84 @@
+package com.redhat.idaas.fhir;
+
+public class FHIRResource {
+
+    String resourceId;
+    String excahngeId;
+    String resource;
+    String payload;
+    String synchronous;
+    String channel;
+    String format;
+    String correlationId;
+    String operation;
+
+    private boolean processed;
+
+    public FHIRResource() {
+
+    }
+
+    public boolean isProcessed() {
+        return processed;
+    }
+
+    public void setProcessed(boolean processed) {
+        this.processed = processed;
+    }
+
+    public String getResourceId() {
+        return resourceId;
+    }
+    public void setResourceId(String resourceId) {
+        this.resourceId = resourceId;
+    }
+    public String getExcahngeId() {
+        return excahngeId;
+    }
+    public void setExcahngeId(String excahngeId) {
+        this.excahngeId = excahngeId;
+    }
+    public String getResource() {
+        return resource;
+    }
+    public void setResource(String resource) {
+        this.resource = resource;
+    }
+    public String getPayload() {
+        return payload;
+    }
+    public void setPayload(String payload) {
+        this.payload = payload;
+    }
+    public String getSynchronous() {
+        return synchronous;
+    }
+    public void setSynchronous(String synchronous) {
+        this.synchronous = synchronous;
+    }
+    public String getChannel() {
+        return channel;
+    }
+    public void setChannel(String channel) {
+        this.channel = channel;
+    }
+    public String getFormat() {
+        return format;
+    }
+    public void setFormat(String format) {
+        this.format = format;
+    }
+    public String getCorrelationId() {
+        return correlationId;
+    }
+    public void setCorrelationId(String correlationId) {
+        this.correlationId = correlationId;
+    }
+    public String getOperation() {
+        return operation;
+    }
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+    
+}
