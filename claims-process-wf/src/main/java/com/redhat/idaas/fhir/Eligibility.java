ec3c0091268782a011bccb2c565fe3c390c95ddc
Pieter Malan <pmalan@redhat.com>
2021-10-28 10:13:31 -0700

    Claims Process Quarkus

--- claims-process-wf/src/main/java/com/redhat/idaas/fhir/Eligibility.java
+++ claims-process-wf/src/main/java/com/redhat/idaas/fhir/Eligibility.java
@@ -6,7 +6,7 @@ import org.hl7.fhir.r4.model.Claim;
 
 
 @ApplicationScoped
-public class Eligibilty {
+public class Eligibility {
 
     public boolean checkEligibility(Claim claim) {
         return true;
